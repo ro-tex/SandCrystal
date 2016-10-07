2ff0634c4186bde4ecca24893518134d1039924e
Ivaylo Novakov <inovakov@gmail.com>
2016-10-07 12:56:41 +0300

    Fixed some typos.

--- spec/fizzbuzz_spec.cr
+++ spec/fizzbuzz_spec.cr
@@ -7,13 +7,13 @@ describe Fizzbuzz do
   it "should divide 3 by 3" do
     Fizzbuzz.div_by_three(3).should eq(true)
   end
-  it "should divide 3 by 5" do
+  it "shouldn't divide 3 by 5" do
     Fizzbuzz.div_by_five(3).should eq(false)
   end
   it "should divide 15 by 5" do
     Fizzbuzz.div_by_five(15).should eq(true)
   end
-  it "should divide 3 by 15" do
+  it "shouldn't divide 3 by 15" do
     Fizzbuzz.div_by_fifteen(3).should eq(false)
   end
   it "should divide 30 by 15" do
