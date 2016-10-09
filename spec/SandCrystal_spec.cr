d899d2b1db83037472280511a84c929b0478bfe5
ro-tex <inovakov@gmail.com>
2016-10-09 15:21:38 +0300

    Now we're actually testing something. Strange that crystal doesn't have "setup" or "before" - maybe I haven't found the right way to use those, yet.

--- spec/SandCrystal_spec.cr
+++ spec/SandCrystal_spec.cr
@@ -1,13 +1,15 @@
 require "./spec_helper"
 
+sand = SandCrystal::Sand.new
+
 describe SandCrystal do
   describe ".hi" do
     it "should print a message" do
-      SandCrystal.hi.should eq(nil)
+      sand.hi.should eq("Hello!")
     end
     context "when passed a number" do
       it "should print it in >>> and <<<" do
-        SandCrystal.hi.should eq(nil)
+        sand.hi(5).should eq(">>> 5 <<<")
       end
     end
   end
