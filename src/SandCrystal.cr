19a5f14f10c5adfe3dc211eb45131f757a64324a
ro-tex <inovakov@gmail.com>
2016-10-09 15:13:48 +0300

    OK, that new test now passes due to those static methods defines in the module.

--- src/SandCrystal.cr
+++ src/SandCrystal.cr
@@ -1,7 +1,16 @@
 require "./SandCrystal/*"
 
 module SandCrystal
-  def hi
+  def self.hi
     puts "Hello!"
   end
+
+  def self.hi(msg : String)
+    puts msg
+  end
+
+  def self.hi(msg : Number)
+    puts ">>> #{msg.to_s} <<<"
+  end
 end
+
